555f10ef3f7f25d4f1cc464b03be173e0e6a93f9
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-20 15:14:04 +0530

    Update i2c.adb

--- src/i2c.adb
+++ src/i2c.adb
@@ -8,7 +8,7 @@ with Ada.Text_IO.Unbounded_IO;
 
 package body i2c is
     R1,R2,V1: Integer;
-    W1: String:="i2cset -y -a 1 0x77 0xF4 0x2E";
+    W1: String:="i2cset -y 1 0x77 0xF4 0x2E";
     --W2: String:="i2cset -y -a 1 0x5d 0x13 0x01";
     --W3: String:="i2cset -y -a 1 0x5d 0x14 0x9";
    
@@ -35,7 +35,7 @@ package body i2c is
             return C_System (Interfaces.C.To_C (Cmd));        
         end System;
         pragma Inline (System);
-	R2: String:="i2cget -y -a 1 0x77" & D;
+	R2: String:="i2cget -y 1 0x77" & D;
     begin
     V1 := System (R2);
     return V1;
