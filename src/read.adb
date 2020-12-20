a34f4f624e4f1fc7b50b9f6cb80d39d7bd4873e9
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-20 15:04:41 +0530

    Update read.adb

--- src/read.adb
+++ src/read.adb
@@ -4,8 +4,9 @@ with Ada.Integer_Text_IO;
 with i2c;
 
 procedure Read is 
+Result: Integer;
 begin
   i2c.write;
   Result := i2c.read("0xF6");
-  Put (Result);
+  Put_Line (Integer'Image(Result));
 end Read;
