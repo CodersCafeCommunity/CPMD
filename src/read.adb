241147b15d4b7ada0ef2359321a6f3a4a9de2468
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-20 15:07:17 +0530

    Update read.adb

--- src/read.adb
+++ src/read.adb
@@ -1,6 +1,6 @@
-with Text_IO;
-with Ada.Text_IO;
-with Ada.Integer_Text_IO;
+with Text_IO; use Text_IO;
+with Ada.Text_IO;use Ada.Text_IO;
+with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
 with i2c;
 
 procedure Read is 
