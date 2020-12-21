7ed4734f618942f685a7969a4a4af96b6ea5ae0f
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-21 09:25:11 +0530

    Update read.adb

--- src/read.adb
+++ src/read.adb
@@ -2,7 +2,7 @@ with Text_IO; use Text_IO;
 with Ada.Text_IO;use Ada.Text_IO;
 with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
-with Ada.Text_IO.Unbounded_IO;
+with Ada.Text_IO.Unbounded_IO;use Ada.Text_IO.Unbounded_IO;
 with i2c;
 
 procedure Read is 
@@ -10,5 +10,5 @@ Result: Unbounded_String;
 begin
   i2c.write("0x77","0xF4","0x2E");
   Result := i2c.read("0x77","0xF6");
-  Put(Result);
+  Put_Line(Result);
 end Read;
