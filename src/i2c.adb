ea423e0957b436abcfbc90e1b66f1c47a15c9e30
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-21 08:42:23 +0530

    Update i2c.adb

--- src/i2c.adb
+++ src/i2c.adb
@@ -7,11 +7,8 @@ with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO;
 
 package body i2c is
-    R1,V1: Integer;
-    R3: Unbounded_String;
-    W1: String:="i2cset -y -a 1 0x77 0xF4 0x2E";
-
-    procedure write is
+    Result: Integer;
+    procedure write(Chip_Address:String, Register_Address: String, Data:String ) is
         function System (Cmd : String) return Integer is
             function C_System (S : Interfaces.C.char_array) return Integer;
         pragma Import (C, C_System, "system");
@@ -19,11 +16,13 @@ package body i2c is
             return C_System (Interfaces.C.To_C (Cmd));        
         end System;
         pragma Inline (System);
+        W0: String:="i2cset -y -a 1 ";
     begin
-    R1 := System (W1);
+    W := To_Unbounded_String(W0) & " "& To_Unbounded_String(Chip_Address) & " " & To_Unbounded_String(Register_Address) & " " & To_Unbounded_String(Data);
+    Result := System (To_String(W));
     end write;
     
-    function read(D: String)  return Integer is
+    function read(Chip_Address:String, Data: String)  return Integer is
           function System (Cmd : String) return Integer is
             function C_System (S : Interfaces.C.char_array) return Integer;
         pragma Import (C, C_System, "system");
@@ -31,10 +30,11 @@ package body i2c is
             return C_System (Interfaces.C.To_C (Cmd));        
         end System;
         pragma Inline (System);
-	R2: String:= "i2cget -y -a 1 0x77 ";
+	    R2: String:= "i2cget -y -a 1 ";
     begin
-    R3 := To_Unbounded_String(R2) & To_Unbounded_String(D) & To_Unbounded_String(" b");
-    V1 := System (To_String(R3));
+    R3 := To_Unbounded_String(R2) & To_Unbounded_String(D);
+    R4 := R3 & To_Unbounded_String(" b");
+    V1 := System (To_String(R4));
     return V1;
     end read;
 
