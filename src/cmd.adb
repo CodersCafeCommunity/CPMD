52223c8252b7ad916233b4cc86327c7c88481f1e
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-29 16:01:19 +0530

    Update cmd.adb

--- src/cmd.adb
+++ src/cmd.adb
@@ -3,7 +3,7 @@ with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 --with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
 --with Ada.Text_IO.Unbounded_IO;
 
---with Ada.Text_IO;            use Ada.Text_IO;
+with Ada.Text_IO;            use Ada.Text_IO;
 --with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 with GNAT.Expect;            use GNAT.Expect;
 with GNAT.OS_Lib;            use GNAT.OS_Lib;
