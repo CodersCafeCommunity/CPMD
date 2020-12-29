6ea1c25965ddbc8111678aecac1e6019e2f5e924
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-29 16:00:34 +0530

    Update cmd.adb

--- src/cmd.adb
+++ src/cmd.adb
@@ -1,13 +1,13 @@
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
-with Ada.Integer_Text_IO;
-with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
-with Ada.Text_IO.Unbounded_IO;
+--with Ada.Integer_Text_IO;
+--with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
+--with Ada.Text_IO.Unbounded_IO;
 
-with Ada.Text_IO;            use Ada.Text_IO;
-with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
+--with Ada.Text_IO;            use Ada.Text_IO;
+--with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 with GNAT.Expect;            use GNAT.Expect;
 with GNAT.OS_Lib;            use GNAT.OS_Lib;
-with GNAT.String_Split;      use GNAT.String_Split;
+--with GNAT.String_Split;      use GNAT.String_Split;
  
 procedure System_Command is
    Command    : String          := "i2cget -y -a 1 0x77 0xF6";
