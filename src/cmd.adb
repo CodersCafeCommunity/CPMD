76bcfe18570ef7ab0b34b959e8d1f8cc3bf27090
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2020-12-29 16:05:25 +0530

    Update cmd.adb

--- src/cmd.adb
+++ src/cmd.adb
@@ -1,25 +1,19 @@
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
---with Ada.Integer_Text_IO;
---with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
---with Ada.Text_IO.Unbounded_IO;
-
 with Ada.Text_IO;            use Ada.Text_IO;
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 with GNAT.Expect;            use GNAT.Expect;
 with GNAT.OS_Lib;            use GNAT.OS_Lib;
 with GNAT.String_Split;      use GNAT.String_Split;
+package system is
+    function is
+        Command    : String          := "i2cget -y -a 1 0x77 0xF6";
+        Args       : Argument_List_Access;
+        Status     : aliased Integer;
+        Separators : constant String := LF & CR;
+        Reply_List : Slice_Set;
  
-procedure System_Command is
-   Command    : String          := "i2cget -y -a 1 0x77 0xF6";
-   Args       : Argument_List_Access;
-   Status     : aliased Integer;
-   Separators : constant String := LF & CR;
-   Reply_List : Slice_Set;
-   --A : Integer;
-
- 
-begin
-   Args := Argument_String_To_List (Command);
+    begin
+    Args := Argument_String_To_List (Command);
    
    -- execute the system command and get the output in a single string
    declare
@@ -32,10 +26,7 @@ begin
     
    begin
       Free (Args);
-      
-      --A := Integer'Value(Response)+1;
-      
-      Put_Line(Integer'Image(Integer'Value(Replace_Slice (Response, 1,2 ,"16#") & "#")));
+      return(Integer'Value(Replace_Slice (Response, 1,2 ,"16#") & "#"));
    end;
  
 end System_Command;
