33321908dea2b60762587e7ccba1939eccf5fda2
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 21:36:51 +0530

    Up

--- src/mq2.ads
+++ src/mq2.ads
@@ -2,7 +2,7 @@ with Text_IO; use Text_IO;
 with Ada.Text_IO;use Ada.Text_IO;
 with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
 
-package body mq2 is
+package mq2 is
     function getSensorVolt(SensorValue : Float) return Float;
     function getRs_air(SensorVolt : Float) return Float;
     function getR0(Rs_air : Float) return Float;
