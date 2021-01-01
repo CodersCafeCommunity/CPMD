0a0854bbeeab081193fde987d8fbb3b2d1499630
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 10:18:04 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -3,13 +3,14 @@ with Ada.Text_IO;use Ada.Text_IO;
 with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded;use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO;use Ada.Text_IO.Unbounded_IO;
+with GNAT.String_Split;      use GNAT.String_Split;
 with cmd; use cmd;
 
-procedure Read is 
+procedure Main is 
   Result: Slice_set;
     begin
-      Result := cmd.execute("ls -l")
+      Result := cmd.execute("ls -l");
       for I in 1 .. Slice_Count (Result) loop
       Put_Line (Slice (Result, I));
       end loop;
-end Read;
+end Main;
