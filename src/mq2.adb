732b11d4f64ee1a0139f98c3123864b6c5a6bd48
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 21:38:52 +0530

    Update mq2.adb

--- src/mq2.adb
+++ src/mq2.adb
@@ -2,20 +2,20 @@ with Text_IO; use Text_IO;
 with Ada.Text_IO;use Ada.Text_IO;
 with Ada.Integer_Text_IO;use Ada.Integer_Text_IO;
 
-package mq2 is
+package body mq2 is
     function getSensorVolt(SensorValue : Float) return Float is
         SensorVolt  :  Float;
         Vin : Float := 3.0;
         begin
             SensorVolt := (SensorValue*Vin)/Float(1023);
             return SensorVolt;
-        end SensorVolt;
+        end getSensorVolt;
     
     function getRs_air(SensorVolt : Float) return Float is
         Rs_air : Float;
         Vin    : Float := 3.0;
         begin
-            Rs_air := (Vin-SensorVolt)/SensorVolt
+            Rs_air := (Vin-SensorVolt)/SensorVolt;
             return Rs_air;
         end getRs_air;
 
