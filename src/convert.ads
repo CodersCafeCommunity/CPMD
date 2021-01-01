1b9a8c67eb14d85dcbf92ab134c1449bfc8857e4
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 20:17:40 +0530

    Update convert.ads

--- src/convert.ads
+++ src/convert.ads
@@ -3,7 +3,7 @@ with Ada.Text_IO;       use Ada.Text_IO;
 
 package convert is
     function hex2int(hex : String) return Integer;
-    function string2int(string : String) return Integer;
+    function string2int(input : String) return Integer;
     function int2string(int : Integer) return String;
-    function string2float(string : String) return Float;
+    function string2float(input : String) return Float;
 end convert;
