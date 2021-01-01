e9b3e7c572b271f6b823ce8bdf831dcbc0f16c6b
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 20:11:09 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -14,7 +14,7 @@ procedure read is
       DELAY 3.0;
       Result := cmd.execute("cat /dev/ttyACM0");
       for I in 1 .. Slice_Count(Result) loop
-      Put_Line (Slice(Result, I));
+      Put_Line (FLoat'Image(Float(Slice(Result, I))));
       end loop;
       end loop;
 end read;
