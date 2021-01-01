374bb4c27e2dc68ac1430461e2ddc86e83d938f5
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 11:54:48 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -9,7 +9,7 @@ with cmd; use cmd;
 procedure read is 
   Result0, Result1 : Slice_set;
     begin
-      Result := cmd.execute("stty -F /dev/ttyACM0 115200 -xcase -icanon min 0 time 3");
+      Result0 := cmd.execute("stty -F /dev/ttyACM0 115200 -xcase -icanon min 0 time 3");
       Result1 := cmd.execute("cat < /dev/ttyACM0");
       for I in 1 .. Slice_Count(Result1) loop
       Put_Line (Slice(Result1, I));
