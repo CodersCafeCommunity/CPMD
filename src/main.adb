cfb7a0ee6c3fed0b02bc1c5266cc7c1c5a803743
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 21:55:46 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -11,18 +11,23 @@ with mq2; use mq2;
 procedure read is
   Result: Slice_set;
   Value,Volt, Rs, R0 : Float;
+  R : Float := 0.0;
     begin
-    --loop
+    for I in 1..20 loop
       Result := cmd.execute("stty -F /dev/ttyACM0 115200 -xcase -icanon min 0 time 3");
       DELAY 3.0;
       Result:= cmd.execute("cat /dev/ttyACM0");
       Value := string2float(Slice(Result, 1));
+      R := R + Value;
+      Put_Line(Integer'Image(I));
+    end loop;
+      Value := R/Float(20);
       Volt  := getSensorVolt(Value);
       Put_Line(Float'Image(Volt));
       Rs    := getRs_air(Volt);
       Put_Line(Float'Image(Rs));
       R0    := getR0(Rs);
       Put_Line(Float'Image(R0));
-      --end loop;
+    
 
 end read;
