ea77356d67f3664e38f38c51ce083b6abc3b489c
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 20:19:31 +0530

    Update convert.adb

--- src/convert.adb
+++ src/convert.adb
@@ -1,28 +1,28 @@
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Text_IO;       use Ada.Text_IO;
 
-package convert is
+package convert body is
     function hex2int(hex : String) return Integer is
         begin
             return(Integer'Value(Replace_Slice (hex, 1,2 ,"16#") & "#"));
         end hex2int;
 
-    function string2int(string : String) return Integer is
+    function string2int(input : String) return Integer is
         begin
-            return((Integer'Value(string));
+            return((Integer'Value(string)));
         end string2int;
 
     function int2string(int : Integer) return String is
         begin
-            return((Integer'Image(int));
+            return((Integer'Image(int)));
         end int2string;
     
-    function string2float(string : String) return Float is
+    function string2float(input : String) return Float is
         I : Integer;
         F : Float;
         begin
-            I = Integer'Value(string);
-            F = Float(I);
+            I := Integer'Value(string);
+            F := Float(I);
             return F;
         end string2float;
 end convert;
