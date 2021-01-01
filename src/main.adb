8eaf7922de499e76d265f8333a55f7053cd4e898
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 22:04:17 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -13,7 +13,7 @@ procedure read is
   Value,Volt, Rs, R0 : Float;
   R : Float := 0.0;
     begin
-    for I in 1..20 loop
+    for I in 1..40 loop
       Result := cmd.execute("stty -F /dev/ttyACM0 115200 -xcase -icanon min 0 time 3");
       DELAY 3.0;
       Result:= cmd.execute("cat /dev/ttyACM0");
