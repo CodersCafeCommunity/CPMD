5c314e2d73eeb04b587c8313f97d1eb221adec6f
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-01 19:57:52 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -11,7 +11,7 @@ procedure read is
   Result: Slice_set;
     begin
       Result0 := cmd.execute("stty -F /dev/ttyACM0 115200 -xcase -icanon min 0 time 3");
-      DELAY 2.0;
+      --DELAY 2.0;
       Result := cmd.execute("cat /dev/ttyACM0");
       for I in 1 .. Slice_Count(Result) loop
       Put_Line (Slice(Result, I));
