f5aba04861004f94e3f50033b9c7a9eec5a0d76a
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-02 11:03:24 +0530

    Update mq2.ads

--- src/mq2.ads
+++ src/mq2.ads
@@ -6,7 +6,7 @@ package mq2 is
     function getSensorVolt(SensorValue : Float) return Float;
     function getRs_air(SensorVolt : Float) return Float;
     function getR0(Rs_air : Float) return Float;
-    function calibrateMQ2(SensorValue : Float) return Float;
+    function calibrateMQ2 return Float;
     function getPPM (R0 : Float; Rs: Float; b: Float; m : Float) return Float;
     function getPPM_AL(SensorValue : Float; R0_air: Float) return Float;
 end mq2;
