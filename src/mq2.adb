8aa3855eec23bb08e04a0f634c99bf33d6531c4c
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-02 11:10:28 +0530

    update

--- src/mq2.adb
+++ src/mq2.adb
@@ -1,5 +1,9 @@
+with Text_IO; use Text_IO;
+with Ada.Text_IO;use Ada.Text_IO;
+with GNAT.String_Split;use GNAT.String_Split;
 with cmd; use cmd;
 with convert; use convert;
+
 
 package body mq2 is
    function getSensorValue return Float is 
--- src/mq2.ads
+++ src/mq2.ads
@@ -1,10 +1,14 @@
+with Text_IO; use Text_IO;
+with Ada.Text_IO;use Ada.Text_IO;
+with GNAT.String_Split;use GNAT.String_Split;
 with cmd; use cmd;
 with convert; use convert;
+
 
 package mq2 is
     function getSensorValue return Float;
     function getSensorVolt(SensorValue : Float) return Float;
-    function getRs_air(SensorVolt : Float) return Float;
+    function getRs(SensorVolt : Float) return Float;
     function getR0(Rs_air : Float) return Float;
     function calibrateMQ2 return Float;
     function getPPM (R0 : Float; Rs: Float; b: Float; m : Float) return Float;
