ac14a0d131420dcd66b1c519726c36f1a5b2ac07
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-03 10:03:33 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -9,19 +9,19 @@ with mq2; use mq2;
 procedure read is
   Result: Slice_set;
   R0_air, SensorValue, PPM_CO,PPM_CH4, PPM_SMOKE : Float;
-  PPM : Integer;
+  PPM0,PPM1,PPM2 : Integer;
     begin
     R0_air := 1.22436;
     Put_Line("R0_air : " & Float'Image(R0_air));
     SensorValue := getSensorValue;
     Put_Line("SensorValue : " & Float'Image(SensorValue));
     PPM_CO := getPPM_CO(SensorValue, R0_air);
-    PPM := Integer(PPM_CO);
-    Put_Line("PPM_CO : " & Integer'Image(PPM)); 
+    PPM0 := Integer(PPM_CO);
+    Put_Line("PPM_CO : " & Integer'Image(PPM0)); 
     PPM_CH4 := getPPM_CH4(SensorValue, R0_air);
-    PPM := Integer(PPM_CO);
-    Put_Line("PPM_CH4 : " & Integer'Image(PPM));
+    PPM1 := Integer(PPM_CH4);
+    Put_Line("PPM_CH4 : " & Integer'Image(PPM1));
     PPM_SMOKE := getPPM_SMOKE(SensorValue, R0_air);
-    PPM := Integer(PPM_SMOKE);
-    Put_Line("PPM_SMOKE : " & Integer'Image(PPM));
+    PPM2 := Integer(PPM_SMOKE);
+    Put_Line("PPM_SMOKE : " & Integer'Image(PPM2));
 end read;
