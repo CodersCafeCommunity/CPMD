004eb2915b12de1f519da98733730bcf02bbb3a5
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-04 15:23:16 +0530

    Update googlesheet.ads

--- src/googlesheet.ads
+++ src/googlesheet.ads
@@ -9,6 +9,6 @@ with convert; use convert;
 package googlesheet is
 
     function log (cURL: Unbounded_String) return Slice_Set;
-    function buildcURL(Time:Sting;Temperature:Integer; Sound:Integer; PPM_CO:Integer; PPM_CH4:Integer; PPM_SMOKE:Integer ) return Unbounded_String;
+    function buildcURL(Time:String;Temperature:Integer; Sound:Integer; PPM_CO:Integer; PPM_CH4:Integer; PPM_SMOKE:Integer ) return Unbounded_String;
 
 end googlesheet;
