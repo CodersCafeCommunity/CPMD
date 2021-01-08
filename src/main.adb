8fb7ccaf3da06c0bddf31e2bc279743bbea87296
Shebin Jose Jacob <shebinjosejacob@gmail.com>
2021-01-08 15:34:21 +0530

    Update main.adb

--- src/main.adb
+++ src/main.adb
@@ -18,12 +18,15 @@ procedure main is
     PPM_Avg_CH4_Array   : PPM_Array := (1..24 => 0.0);
     PPM_Avg_SMOKE_Array : PPM_Array := (1..24 => 0.0);
     PPM_Mean_CO_Array   : PPM_Array := (1..24 => 0.0);
+    PPM_Mean_CH4_Array  : PPM_Array := (1..24 => 0.0);
+    PPM_Mean_SMOKE_Array  : PPM_Array := (1..24 => 0.0);
 
     Res : Slice_set;
     R0_air, SensorValue, dB, dC: Float;
     PPM_CO, PPM_CH4, PPM_SMOKE :Integer;
     PPM_Sum_CO,PPM_Sum_CH4,PPM_Sum_SMOKE :Float:= 0.0;
     PPM_Avg_CO,PPM_Avg_CH4,PPM_Avg_SMOKE :Float:= 0.0;
+    PPM_CH4_Final,PPM_SMOKE_Final,PPM_CO_Final,PPM_Final : Float;
     i : Integer := 1;
     Count_Time :Integer := 0;
     cURL,Time: Unbounded_String;
@@ -35,11 +38,11 @@ procedure main is
         R0_air := 1.22436;
         SensorValue := mq2.getSensorValue;
         PPM_CO   := Integer(mq2.getPPM_CO(SensorValue, R0_air));
-        Put_Line("PPM_CO : " & Integer'Image(PPM_CO)); 
+        --Put_Line("PPM_CO : " & Integer'Image(PPM_CO)); 
         PPM_CH4  := Integer(mq2.getPPM_CH4(SensorValue, R0_air));
-        Put_Line("PPM_CH4 : " & Integer'Image(PPM_CH4));
+        --Put_Line("PPM_CH4 : " & Integer'Image(PPM_CH4));
         PPM_SMOKE:= Integer(mq2.getPPM_SMOKE(SensorValue, R0_air));
-        Put_Line("PPM_SMOKE : " & Integer'Image(PPM_SMOKE));
+        --Put_Line("PPM_SMOKE : " & Integer'Image(PPM_SMOKE));
 
         PPM_Sum_CO := PPM_Sum_CO + Float(PPM_CO);
         PPM_Sum_CH4 := PPM_Sum_CH4 + FLoat(PPM_CH4);
@@ -48,12 +51,12 @@ procedure main is
         ----- Sound Sensor -----
         SensorValue := sound.getSensorValue;
         dB := sound.getdB(SensorValue);
-        Put_Line("Intensity in dB :" & Float'Image(dB));
+       -- Put_Line("Intensity in dB :" & Float'Image(dB));
 
         ----- Temperature Sensor -----
         SensorValue := lm35.getSensorValue;
         dC := lm35.getdC(SensorValue);
-        Put_Line("Temp in *C :" & Float'Image(dC));
+        --Put_Line("Temp in *C :" & Float'Image(dC));
 
         ------ Time ------
         Time := To_Unbounded_String(mytime.getTime);
@@ -65,29 +68,75 @@ procedure main is
         Delay 1.0;
         Count_Time:= Count_Time + 1;
     
-        if Count_Time = 60 then
-            PPM_Avg_CO := PPM_Sum_CO/60.0;
+        if Count_Time = 3600 then
+            PPM_Avg_CO := PPM_Sum_CO/3600.0;
             PPM_Avg_CO_Array(i) := PPM_Avg_CO;
-        
-            -- PPM_Avg_CH4 := PPM_Sum_CH4 /3600;
-            -- PPM_Avg_CH4_Array(i) := PPM_Avg_CH4;
+            
+            PPM_Avg_CH4 := PPM_Sum_CH4 /3600.0;
+            PPM_Avg_CH4_Array(i) := PPM_Avg_CH4;
 
-            --PPM_Avg_SMOKE := PPM_Avg_SMOKE / 3600;
-            --PPM_Avg_SMOKE_Array(i) := PPM_Avg_SMOKE;
-            i := i + 1;
+            PPM_Avg_SMOKE := PPM_Avg_SMOKE / 3600.0;
+            PPM_Avg_SMOKE_Array(i) := PPM_Avg_SMOKE;
+
             if i = 24 then
+                -- Carbon Monoxide
                 for I in 1..17 loop 
                     for J in I..I+7 loop 
                         PPM_Mean_CO_Array(I):= PPM_Mean_CO_Array(I) + PPM_Avg_CO_Array(J);
                     end loop;
                 end loop;
-
                 for I in 1..17 loop
-                    Put_Line(Float'Image(PPM_Mean_CO_Array(I)));
+	                  if PPM_Mean_CO_Array(1) < PPM_Mean_CO_Array(I+1) then
+                       PPM_Mean_CO_Array(1) := PPM_Mean_CO_Array(I+1);
+                    end if;
+                    PPM_CO_Final := PPM_Mean_CO_Array(1);
+                    Put_Line("Max CO Value :" & Float'Image(PPM_CO_Final));
                 end loop;
-
+                
+                -- Methane
+                for I in 1..17 loop 
+                    for J in I..I+7 loop 
+                        PPM_Mean_CH4_Array(I):= PPM_Mean_CH4_Array(I) + PPM_Avg_CH4_Array(J);
+                    end loop;
+                end loop;
+                for I in 1..17 loop
+	                  if PPM_Mean_CH4_Array(1) < PPM_Mean_CH4_Array(I+1) then
+                       PPM_Mean_CH4_Array(1) := PPM_Mean_CH4_Array(I+1);
+                    end if;
+                    PPM_CH4_Final := PPM_Mean_CH4_Array(1);
+                    Put_Line("Max CH4 Value :" & Float'Image(PPM_CH4_Final));
+                end loop;
+                
+                -- Smoke
+                for I in 1..17 loop 
+                    for J in I..I+7 loop 
+                        PPM_Mean_SMOKE_Array(I):= PPM_Mean_SMOKE_Array(I) + PPM_Avg_SMOKE_Array(J);
+                    end loop;
+                end loop;
+                for I in 1..17 loop
+	                  if PPM_Mean_SMOKE_Array(1) < PPM_Mean_SMOKE_Array(I+1) then
+                       PPM_Mean_SMOKE_Array(1) := PPM_Mean_SMOKE_Array(I+1);
+                    end if;
+                    PPM_SMOKE_Final := PPM_Mean_SMOKE_Array(1);
+                    Put_Line("Max SMOKE Value :" & Float'Image(PPM_SMOKE_Final));
+                end loop;
+                PPM_Final := (PPM_CH4_Final + PPM_SMOKE_Final + PPM_CO_Final)/3.0;
+                case PPM_Final is
+                  when 0.0 .. 200.0 =>
+                    Put_Line ("Excellent Air quality");
+                    cmd.execute("gpio -p write 14 high");
+                  when 201.0 .. 400.0 =>
+                    Put_Line ("Moderate Air quality"); 
+                    cmd.execute("gpio -p write 15 high");
+                  when others =>
+                    Put_Line ("Severe Air quality"); 
+                    cmd.execute("gpio -p write 18 high");
+                    cmd.execute("gpio -p write 18 high");
+                    exit;
+                i := 0;
             end if;
             Count_Time:= 0;
+          i := i + 1;
         end if;
     end loop;
 end main;
